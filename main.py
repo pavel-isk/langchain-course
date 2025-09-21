5f866ae814e1db9f9333e16986648ec6fab61f31
Pavel Iskandarov <pavel.iskandarov@alcon.com>
2025-09-21 23:57:26 +0300

    Psycho bot

--- main.py
+++ main.py
@@ -25,25 +25,37 @@ def main():
     #     temperature=0,
     #     max_retries=2,
     # )
-    llm = ChatYandexGPT(folder_id=os.getenv("YC_FOLDER"), temperature=0.3, model_name="yandexgpt-5-lite")
+    llm = ChatYandexGPT(folder_id=os.getenv("YC_FOLDER"), temperature=0.3, model_name="yandexgpt-5-pro")
     # llm = ChatOllama(
     #     temperature=0.3,
     #     model="gemma3:270m"
     # )
 
     information = """
-    Jeff Bezos is an American entrepreneur and investor best known as the founder and former CEO of Amazon, the world's largest online retailer.
-    He also owns The Washington Post and founded Blue Origin, a private aerospace manufacturer and sub-orbital spaceflight services company.
-    Bezos is recognized for revolutionizing e-commerce and cloud computing, and is one of the wealthiest individuals globally.
+    Вот список распространённых ошибок мышления, характерных для депрессивного состояния:
+    1. Катастрофизация — ожидание худшего исхода событий.
+    2. Чёрно-белое мышление — видение ситуации только в крайностях, без нюансов.
+    3. Обесценивание позитивного — игнорирование или преуменьшение своих достижений и положительных событий.
+    4. Персонализация — принятие на себя чрезмерной ответственности за негативные события.
+    5. Обобщение — вывод глобальных негативных выводов на основе единичных случаев.
+    6. Фильтрация — сосредоточение только на негативных деталях, игнорируя позитивные.
+    7. Эмоциональное рассуждение — выводы на основе эмоций, а не фактов.
+    8. Предсказание будущего — уверенность, что всё будет плохо, без объективных оснований.
+    9. Чтение мыслей — предположение, что окружающие думают о вас негативно.
+    10. Навешивание ярлыков — использование негативных определений для себя или других.
     """
 
-    task = "Describe the person in a short sentence. And provide a numeric list with two piculiar facts about him."
-
-    template = "Given the information {information} about a person, implement the task: {task}"
+    template = """
+        Ты бот помощник, который помогает людям распознавать ошибки эмоционального мышления и предлагает рациональные альтернативы.
+        Используя информацию об ошибках эмоционального мышления человека: {information},
+        предложи, какие ошибки совершает человек в следующей ситуации, мыслях и реакции: {situation}.
+        Предложи рациональные альтернативные мысли и реакции.
+        Ответ должен быть на русском языке.
+    """
     prompt_template = PromptTemplate(
-        input_variables=["information", "task"],
+        input_variables=["information", "situation"],
         template=template,
-    )
+    ).partial(information=information)
 
     # prompt = prompt_template.format(information=information, task=task)
     # response = llm.invoke([
@@ -53,10 +65,16 @@ def main():
 
     chain = prompt_template | llm
 
-    response = chain.invoke(input={"information": information, "task": task})
 
-    print("Response:")
-    print(response.content)
+    # situation = "Коллега по работе не поздоровался со мной утром. Наверное, он меня не любит и хочет уволить. Я зажимаюсь в плечах и чувствую себя подавленным."
+    situation = input("Напиши что у тебя случилось: ")
+
+    while situation != "пока":
+        response = chain.invoke(input={"situation": situation})
+        print("Response:")
+        print(f"\033[92m{response.content}\033[0m")
+
+        situation = input("Напиши что думаешь или пока: ")
 
 
 if __name__ == "__main__":
