ba4c292624700c0ff14e9376c6d5602f8d9f307e
Pavel Iskandarov <pavel.iskandarov@alcon.com>
2025-09-23 21:48:14 +0300

    make use of schemas and update main code

--- main.py
+++ main.py
@@ -5,46 +5,89 @@ and generate responses based on a given prompt.
 """
 
 import os
+
 # import json
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
 from langchain_community.chat_models import ChatYandexGPT
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 
 from prompt import GET_RATIONAL_THOUGHTS_PROMPT
-from schemas import RationalThough, CognitiveMistakes, Attitude, AgentResponse
-
+from schemas import AgentResponse
 
 load_dotenv()
 
 
-def main(): # Make use of Pydantic schemas in your agent logic
+def main():  # Make use of Pydantic schemas in your agent logic
     print("Starting Hello world project...")
 
     llm = ChatYandexGPT(folder_id=os.getenv("YC_FOLDER"), temperature=0.5, model_name="yandexgpt-5-pro")
+    output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 
-    information = ""
-    with open("data/app_data.json", "r", encoding="utf-8") as f:
-        information = f.read()
+    information = load_app_data()
+    extract_output = RunnableLambda(lambda x: output_parser.parse(x.content))
 
     template = GET_RATIONAL_THOUGHTS_PROMPT
     prompt_template = PromptTemplate(
         input_variables=["information", "user_case"],
         template=template,
-    ).partial(information=information)
+    ).partial(
+        information=information,
+        format_instructions=output_parser.get_format_instructions(),
+    )
 
-    chain = prompt_template | llm
+    chain = prompt_template | llm | extract_output
+
     user_case = {
-        "Ситуация": input("Опиши ситуацию: "),
-        "Мысли": input("Опиши мысли: "),
-        "Эмоции": input("Опиши эмоции и их интенсивность по 10-бальной шкале: "),
-        "Телесная реакция": input("Опиши телесную реакцию: "),
-        "Поведение": input("Опиши поведение: "),
+        # "Ситуация": input("Опиши ситуацию: "),
+        # "Мысли": input("Опиши мысли: "),
+        # "Эмоции": input("Опиши эмоции и их интенсивность по 10-бальной шкале: "),
+        # "Телесная реакция": input("Опиши телесную реакцию: "),
+        # "Поведение": input("Опиши поведение: "),
+        "Ситуация": "Я опоздал на работу, потому что проспал.",
+        "Мысли": "Я всегда опаздываю, я неорганизованный человек и меня уволят. Важно чтобы никто не узнал, что я опоздал.",
+        "Эмоции": "Тревога 8, стыд 7",
+        "Телесная реакция": "Сердцебиение, потливость, напряжение в плечах",
+        "Поведение": "Я быстро оделся и помчался на работу, не позвонив никому и не предупредив.",
     }
 
     user_case_str = "\n".join([f"{k}: {v}" for k, v in user_case.items()])
     response = chain.invoke(input={"user_case": user_case_str})
+
+    print_response(response)
+
+
+def load_app_data():
+    information = ""
+    with open("data/app_data.json", "r", encoding="utf-8") as f:
+        information = f.read()
+    return information
+
+
+# Function that prints response in human readable format with colors
+def print_response(response: AgentResponse):
     print("Response:")
-    print(f"\033[92m{response.content}\033[0m")
+
+    print(f"\033[1;97m{response.summary}\033[0m")
+
+    sorted_mistakes = sorted(response.mistakes, key=lambda x: x.probability, reverse=True)
+    print(f"\033[38;5;208m Ошибки мышления\033[0m")
+    for idx, mistake in enumerate(sorted_mistakes, 1):
+        print(f"\033[38;5;208m{idx}. {mistake.name}: {mistake.explanation}\033[0m")
+
+    
+    print(f"\033[91m Установки\033[0m")
+    for idx, attitude in enumerate(response.attitudes, 1):
+        print(f"\033[91m{idx}. {attitude.content}\033[0m")
+
+    print(f"\033[94m Рациональные мысли\033[0m")
+    for idx, thought in enumerate(response.rationalization, 1):
+        print(f"\033[94m{idx}. {thought.content}\033[0m")
+
+    print(f"\033[92m Позитивные моменты в ситуации\033[0m")
+    for idx, moment in enumerate(response.positivism, 1):
+        print(f"\033[92m{idx}. {moment.content}\033[0m")
 
 
 if __name__ == "__main__":
--- schemas.py
+++ schemas.py
@@ -3,25 +3,37 @@ from typing import List
 from pydantic import BaseModel, Field
 
 
+class CognitiveMistakes(BaseModel):
+    name: str = Field(description="Когнитивная ошибка, выявленная в описании случая клиента.")
+    explanation: str = Field(
+        description="Краткое объяснение почему эта когнитивная ошибка применима к описанию случая клиента."
+    )
+    probability: float = Field(
+        description="Вероятность того, что когнитивная ошибка верна для описания случая клиента.", ge=0.0, le=1.0
+    )
+
+
 class Attitude(BaseModel):
     content: str = Field(description="Мысленная установка к себе, людям и миру, взятая из описания случая клиента.")
     probability: float = Field(
-        description="Вероятность того, что установка верна для описания случая клиента.",
-        ge=0.0, le=1.0
+        description="Вероятность того, что установка верна для описания случая клиента.", ge=0.0, le=1.0
     )
+
 
 class RationalThough(BaseModel):
-    content: str = Field(description="Рациональная мысль, корректирующая иррациональные мысли из описания случая клиента.")
+    content: str = Field(
+        description="Рациональная мысль, корректирующая иррациональные мысли из описания случая клиента."
+    )
     probability: float = Field(
-        description="Вероятность того, что мысль верна для описания случая клиента.",
-        ge=0.0, le=1.0
+        description="Вероятность того, что мысль верна для описания случая клиента.", ge=0.0, le=1.0
     )
 
-class CognitiveMistakes(BaseModel):
-    content: str = Field(description="Когнитивная ошибка, выявленная в описании случая клиента.")
+class PositiveMoments(BaseModel):
+    content: str = Field(
+        description="Позитивные моменты в контексте описанного случая клиента."
+    )
     probability: float = Field(
-        description="Вероятность того, что когнитивная ошибка верна для описания случая клиента.",
-        ge=0.0, le=1.0
+        description="Вероятность того, что момент верен для придания позитивного контекста случая клиента.", ge=0.0, le=1.0
     )
 
 
@@ -29,6 +41,20 @@ class AgentResponse(BaseModel):
     """Schema for the agent's response with answer and sources."""
 
     summary: str = Field(description="Краткое резюме ответа, сгенерированного агентом")
-    sources: List[CognitiveMistakes] = Field(default_factory=list, description="Список когнитивных ошибок, выявленных в описании случая клиента")
-    sources: List[RationalThough] = Field(default_factory=list, description="Список рациональных мыслей, корректирующих иррациональные мысли из описания случая клиента")
-    sources: List[Attitude] = Field(default_factory=list, description="Список мысленных установок к себе, людям и миру, взятых из описания случая клиента")
+    mistakes: List[CognitiveMistakes] = Field(
+        default_factory=list,
+        description="Список когнитивных ошибок, выявленных в описании случая клиента"
+    )
+    rationalization: List[RationalThough] = Field(
+        default_factory=list,
+        description="Список рациональных мыслей, корректирующих иррациональные мысли из описания случая клиента",
+    )
+    attitudes: List[Attitude] = Field(
+        default_factory=list,
+        description="Список мысленных установок к себе, людям и миру, взятых из описания случая клиента",
+    )
+
+    positivism: List[PositiveMoments] = Field(
+        default_factory=list,
+        description="Список позитивных моментов в контексте описанного случая клиента",
+    )
