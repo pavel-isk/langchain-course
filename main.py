3612e701fe0cc98c05773b0fd1f7ba04e19eb961
Pavel Iskandarov <pavel.iskandarov@alcon.com>
2025-09-23 10:11:44 +0300

    split app data

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
 """
 This script demonstrates how to use LangChain with YandexGPT to process information
 and generate responses based on a given prompt.
 """
 
 import os
+# import json
 from dotenv import load_dotenv
+from prompt import GET_RATIONAL_THOUGHTS_PROMPT
 from langchain_core.prompts import PromptTemplate
 
 # from langchain_ollama import ChatOllama
@@ -31,50 +34,29 @@ def main():
     #     model="gemma3:270m"
     # )
 
-    information = """
-    Вот список распространённых ошибок мышления, характерных для депрессивного состояния:
-    1. Катастрофизация — ожидание худшего исхода событий.
-    2. Чёрно-белое мышление — видение ситуации только в крайностях, без нюансов.
-    3. Обесценивание позитивного — игнорирование или преуменьшение своих достижений и положительных событий.
-    4. Персонализация — принятие на себя чрезмерной ответственности за негативные события.
-    5. Обобщение — вывод глобальных негативных выводов на основе единичных случаев.
-    6. Фильтрация — сосредоточение только на негативных деталях, игнорируя позитивные.
-    7. Эмоциональное рассуждение — выводы на основе эмоций, а не фактов.
-    8. Предсказание будущего — уверенность, что всё будет плохо, без объективных оснований.
-    9. Чтение мыслей — предположение, что окружающие думают о вас негативно.
-    10. Навешивание ярлыков — использование негативных определений для себя или других.
-    """
+    information = ""
+    with open("data/app_data.json", "r", encoding="utf-8") as f:
+        information = f.read()
 
-    template = """
-        Ты бот помощник, который помогает людям распознавать ошибки эмоционального мышления и предлагает рациональные альтернативы.
-        Используя информацию об ошибках эмоционального мышления человека: {information},
-        предложи, какие ошибки совершает человек в следующей ситуации, мыслях и реакции: {situation}.
-        Предложи рациональные альтернативные мысли и реакции.
-        Ответ должен быть на русском языке.
-    """
+    template = GET_RATIONAL_THOUGHTS_PROMPT
     prompt_template = PromptTemplate(
-        input_variables=["information", "situation"],
+        input_variables=["information", "user_case"],
         template=template,
     ).partial(information=information)
 
-    # prompt = prompt_template.format(information=information, task=task)
-    # response = llm.invoke([
-    #     SystemMessage(content="You are a helpful assistant that helps people find information."),
-    #     HumanMessage(content=prompt)
-    # ])
-
     chain = prompt_template | llm
+    user_case = {
+        "Ситуация": input("Опиши ситуацию: "),
+        "Мысли": input("Опиши мысли: "),
+        "Эмоции": input("Опиши эмоции и их интенсивность по 10-бальной шкале: "),
+        "Телесная реакция": input("Опиши телесную реакцию: "),
+        "Поведение": input("Опиши поведение: "),
+    }
 
-
-    # situation = "Коллега по работе не поздоровался со мной утром. Наверное, он меня не любит и хочет уволить. Я зажимаюсь в плечах и чувствую себя подавленным."
-    situation = input("Напиши что у тебя случилось: ")
-
-    while situation != "пока":
-        response = chain.invoke(input={"situation": situation})
-        print("Response:")
-        print(f"\033[92m{response.content}\033[0m")
-
-        situation = input("Напиши что думаешь или пока: ")
+    user_case_str = "\n".join([f"{k}: {v}" for k, v in user_case.items()])
+    response = chain.invoke(input={"user_case": user_case_str})
+    print("Response:")
+    print(f"\033[92m{response.content}\033[0m")
 
 
 if __name__ == "__main__":
