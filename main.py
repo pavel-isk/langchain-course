9d884f673889a3d7f25fa3b17158db62f2bb4f9e
Pavel Iskandarov <pavel.iskandarov@alcon.com>
2025-10-08 09:21:37 +0300

    Implement cognitive psycho bot

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ and generate responses based on a given prompt.
 """
 
 import os
+import json
 
 from dotenv import load_dotenv
 from langchain_core.tools import tool
-from langchain_core.messages import HumanMessage, ToolMessage,  AIMessage
+from langchain_core.messages import HumanMessage, ToolMessage,  SystemMessage
 from langchain_gigachat import GigaChat
 # from langchain_community.chat_models import ChatYandexGPT
 from langchain_ollama import ChatOllama
@@ -16,43 +17,66 @@ load_dotenv()
 
 def main():
     print("Starting Hello world project...")
+    cognitive_distortions = None
+    with open("data/app_data.json", "r", encoding="utf-8") as f:
+        t = json.loads(f.read())
+        cognitive_distortions = "\n".join([
+            f"- {k}: {v}" for k, v in t.get("Искажения").items()
+        ])
+
+    system_message = SystemMessage(content="""
+Ты психологический ассистент, который помогает людям работать по методу Когнитивно-поведенческой терапии (КПТ).
+Твоя задача - собрать информацию от пользователя по следующим важным пунктам: 
+- автоматические мысли
+- эмоции с их инетенсивностью по шкале от 0 до 5
+- как он поступил в этой ситуации
+- его физические ощущения.
+Собирай данные постепенно, задавая уточняющие вопросы. Веди разговор как его друг. 
+Не спрашивай все сразу, общение должно казаться живым и естественным.
+Как соберёшь данные, сделай небольшое саммари по ситуации пользователя.
+Затем, используя эту информацию, сообщить пользователю, какие у него когнитивные искажения с объяснением почему они применимы.
+Затем предложить рациональные, более адаптивные мысли, который скорректируют эти искажения.    
+Список когнитивных искажений на который ты должен ориентироваться при анализе ситуации пользователя:
+{cognitive_distortions}.
+""".format(cognitive_distortions=cognitive_distortions.strip()))
 
     # llm = ChatYandexGPT(folder_id=os.getenv("YC_FOLDER"), temperature=0, model_name="yandexgpt-5-pro")
     # llm = ChatOllama(model="llama3.1:8b", temperature=0)
-    llm = GigaChat(credentials=os.getenv("GIGACHAT_CREDENTIALS"), temperature=0, model_name="GigaChat-2", verify_ssl_certs=False)
-    llm_with_tools = llm.bind_tools([count_characters])
+    llm = GigaChat(credentials=os.getenv("GIGACHAT_CREDENTIALS"), temperature=0, model="GigaChat-2-Pro", verify_ssl_certs=False)
+    llm_with_tools = llm.bind_tools([ask_user])
     messages = [
-        HumanMessage("How many characters are in the word 'Hello world'?")
+        system_message,
+        HumanMessage(content="Начинай сбор информации от пользователя."),
     ]
 
     ai_message = llm_with_tools.invoke(messages)
-    print("AI response:", ai_message.content)
+    print("AI response:", ai_message, len(ai_message.tool_calls))
     messages.append(ai_message)
     
     while ai_message.tool_calls:
         for tool_call in ai_message.tool_calls:
             tool_name = tool_call["name"]
             tool_args = tool_call["args"]
-            print(tool_call)
             
-            if tool_name == "count_characters":
-                tool_result = count_characters.invoke(tool_args)
+            if tool_name == "ask_user":
+                tool_result = ask_user.invoke(tool_args)
                 messages.append(ToolMessage(
                     tool_result,
                     tool_call_id=tool_call["id"],
                 ))
                 ai_message = llm_with_tools.invoke(messages)
-                print("AI response:", ai_message.content)
+                messages.append(ai_message)
 
-    print("Final result:", ai_message.content)
+    print(f"\033[94mFinal result:\n{ai_message.content}\033[0m")
 
 
 @tool
-def count_characters(text: str ="") -> int:
-    """Count the number of characters in a given text."""
-    print(f"Counting characters in: {text}")
-    return len(text)
-
+def ask_user(message: str ="") -> int:
+    """
+    Отправляет сообщение от модели message пользователю и возвращает ответ пользователя в виде строки.
+    Этот инструмент должен использоваться LLM всякий раз, когда необходимо пообщаться с пользователем, задать вопросы или получить ввод.
+    """
+    return input(f"\033[92m{message}\033[0m: ")
 
 if __name__ == "__main__":
     main()
