d7956f82d7d5334d03b828580b6cb09816ff2b32
pavel-isk <pavel.isk.ne@gmail.com>
2025-10-16 12:26:16 +0300

    prepare gigachat env

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ from langchain import hub
 from langchain_text_splitters import CharacterTextSplitter
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.chat_models import ChatYandexGPT
-from langchain_community.embeddings.yandex import YandexGPTEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain_gigachat.chat_models import GigaChat
+from langchain_gigachat.embeddings import GigaChatEmbeddings
+from langchain_community.vectorstores import Pinecone
 
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -26,45 +26,35 @@ load_dotenv()
 def main():  # Make use of Pydantic schemas in your agent logic
     print("Starting simple RAG world project...")
 
-    llm = ChatYandexGPT(folder_id=os.getenv("YC_FOLDER"), temperature=0, model_name="yandexgpt-5-pro")
-    embeddings = YandexGPTEmbeddings(folder_id=os.getenv("YC_FOLDER"), iam_token=os.getenv("YC_API_KEY"))
+    llm = GigaChat(credentials=os.getenv("GIGACHAT_CREDENTIALS"), temperature=0, model="GigaChat-2-Pro", verify_ssl_certs=False)
+    embeddings = GigaChatEmbeddings(credentials=os.getenv("GIGACHAT_CREDENTIALS"), temperature=0, model="Embeddings", verify_ssl_certs=False)
+    result = embeddings.embed_documents(texts=["Привет!"])
+    print(result)
+
     # generate_vs_data("sample.pdf", embeddings)
     # generate_vs_data("sample2.pdf", embeddings)
     
     # vectorstore = FAISS.from_documents(docs, embeddings)
     # vectorstore.save_local("faiss_index_sample2")
-    vectorstore = FAISS.load_local(
-        # "data/faiss_index_sample", embeddings, allow_dangerous_deserialization=True
-        "data/faiss_index_sample2", embeddings, allow_dangerous_deserialization=True
-    )
+    # vectorstore = FAISS.load_local(
+    #     # "data/faiss_index_sample", embeddings, allow_dangerous_deserialization=True
+    #     "data/faiss_index_sample2", embeddings, allow_dangerous_deserialization=True
+    # )
 
-    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-    combine_docs_chain = create_stuff_documents_chain(
-        llm, retrieval_qa_chat_prompt
-    )
-    retrieval_chain = create_retrieval_chain(
-        vectorstore.as_retriever(), combine_docs_chain
-    )
+    # retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    # combine_docs_chain = create_stuff_documents_chain(
+    #     llm, retrieval_qa_chat_prompt
+    # )
+    # retrieval_chain = create_retrieval_chain(
+    #     vectorstore.as_retriever(), combine_docs_chain
+    # )
     
-    res = retrieval_chain.invoke(
-        {"input": input("Введи свой тупой вопрос: ")}
-    )
-    print(f"Ответ: {res['answer']}")
+    # res = retrieval_chain.invoke(
+    #     {"input": input("Введи свой тупой вопрос: ")}
+    # )
+    # print(f"Ответ: {res['answer']}")
 
 
-def generate_vs_data(filename: str, embeddings) -> None:
-    pdf_path = f"data/{filename}"
-    documents = PyPDFLoader(pdf_path).load()
-    print(f"Loaded {len(documents)} documents from {pdf_path}")
-
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
-    docs = text_splitter.split_documents(documents)
-    print(f"Split into {len(docs)} chunks of text (max. 1000 characters each)")
-
-    vectorstore = FAISS.from_documents(docs, embeddings)
-    index_path = f"faiss_index_{filename.split('.')[0]}"
-    vectorstore.save_local(f"data/{index_path}")
-    print(f"Vector store saved to data/{index_path}")
 
 if __name__ == "__main__":
     main()
